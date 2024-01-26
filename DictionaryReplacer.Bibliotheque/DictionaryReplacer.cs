987c756828f574b7b0905a4f315899e266ba4597
Thibaud Payen <Thibaudpayenbts@gmail.com>
2024-01-26 14:44:47 +0100

    refactor : string et dictionaire vide

--- DictionaryReplacer.Bibliotheque/DictionaryReplacer.cs
+++ DictionaryReplacer.Bibliotheque/DictionaryReplacer.cs
@@ -4,11 +4,7 @@
     {
         public string Replace(string input, Dictionary<string, string> dictionary)
         {
-            if (input == "")
-            {
-                return input;
-            }
-            if (dictionary == null || dictionary.Count == 0)
+            if (input == "" || dictionary == null || dictionary.Count == 0)
             {
                 return input;
             }
